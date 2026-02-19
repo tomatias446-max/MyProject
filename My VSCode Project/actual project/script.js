596a27a4b93358e0ed2f231150c12c302c8253eb
tomatias446-max <tomatias446@gmail.com>
2026-02-19 08:38:08 +0200

    Add files via upload

---
+++ My VSCode Project/actual project/script.js
@@ -0,0 +1,19 @@
+let turn = 0;
+let num1;
+let num2;
+
+function play(a) {
+    a = Number(a)-1;
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function roll() {
+    num1 = random(1,6);
+    num2 = random(1,6);
+    document.getElementById("dice").innerHTML = num1+","+num2;
+}
